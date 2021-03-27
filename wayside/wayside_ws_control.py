f9bfacb07daa826a08004b6c20fafc36c4248055
Kayla <kayla.m.santos@gmail.com>
2021-03-27 16:58:44 -0400

    update if __name__ == '__main__': 	unittest.main()

--- wayside/wayside_ws_control.py
+++ wayside/wayside_ws_control.py
@@ -1,6 +1,7 @@
 class Wayside:
 	def __init__(self, plcfile):
 		self.plcfile = plcfile
+		self.load_plc()
 		
 	def m_order(self, order):
 		
@@ -17,7 +18,7 @@ class Wayside:
 		linecount = 0
 		swc = 0
 		swr = 0
-		crc
+		crc = 0
 		proc = 0
 		plc = f.readlines()
 		for line in plc[0:]:
@@ -26,19 +27,22 @@ class Wayside:
 				proc = 0
 			#switch
 			elif line[0:2] == "sw" and proc == 0:
-				switch_name[swcount][0] = "Switch " + line[2:]
-				switch_name[swcount][1] = "0" 
+				self.switch_name[swcount][0] = "Switch " + line[2:]
+				self.switch_name[swcount][1] = "0" 
 				swcount = swcount + 1
 			#cross
 			elif line[0:2] == "cr" and proc == 0:
-				cross_name[crcount][0] = "Crossing " + line[2:]
-				cross_name[crcount][1] = "0"
+				self.cross_name[crcount][0] = "Crossing " + line[2:]
+				self.cross_name[crcount][1] = "0"
 				crcount = crcount + 1
 			#block
 			elif line[0:2] == "bl" and proc == 0:
-				block_name[blcount][0] = "Block " + line[2:]
-				block_name[blcount][1] = "0"
+				self.block_name[blcount][0] = "Block " + line[2:]
+				self.block_name[blcount][1] = "0"
 				blcount = blcount + 1
+			#stopping distance
+			elif line[0:2] == "st" and proc == 0:
+				self.stop_distance = int(line[2:])
 			#end
 			elif line == "end var":
 				proc = 1
@@ -49,9 +53,16 @@ class Wayside:
 			print("proc")
 			print(line)
 			if line[0:2] == "sw":
-				sw_connect[swr][0] = [plc[linecount+1], plc[linecount+2]]
-				sw_connect[swr][1] = [plc[linecount+3], plc[linecount+4]]
+				self.sw_connect[swr][0] = [plc[line+1], plc[line+2]]
+				self.sw_connect[swr][1] = [plc[line+3], plc[line+4]]
 				swr = swr + 1
 			if line[0:2] == "cr":
-				cr_connect[crc][0] = plc[linecount+1]
+				self.cr_connect[crc][0] = plc[linecount+1]
+			if line[0:2] == "end proc":
+				break
 		f.close()
+
+		
+if __name__ == '__main__':
+	Wayside.main()
+		
