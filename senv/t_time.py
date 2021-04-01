8d71e1ef88507cdff5e6aadfd6a31ce9e57259f3
Eric <lieric236@gmail.com>
2021-04-01 00:59:41 -0400

    Add files via upload

--- senv/t_time.py
+++ senv/t_time.py
@@ -1,7 +1,6 @@
 import time
 import simpy
 import simpy.rt
-from c_time import mil_tim
 
 #def clock(env,mil):
 #start = time.perf_counter()
@@ -12,6 +11,29 @@ from c_time import mil_tim
 
 class timing():
     def __init__(self):
-        env = simpy.rt.RealtimeEnvironment(factor=1)
-        tim = mil_tim(env)
-        env.run(until=999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999)
+        self.sec = 0
+        self.mint = 0
+        self.hr = 0
+        self.run()
+
+    def run(self):
+        check = 0
+        timer = 0
+        while True:
+            self.sec = timer - (check*60)
+            if self.sec == 60:
+                self.mint = self.mint+1
+                self.sec = 0
+                check = check+1
+                if self.mint ==  60:
+                    self.hr = self.hr+1
+                    self.mint = 0
+                    if self.hr == 24:
+                        self.hr = 0
+            print('The time is ' + str(self.hr)+ ':' + str(self.mint) + ':' +str(self.sec))
+            signals.time.emit(self.sec,self.mint,self.hr,self.env.now)
+            timer += 1
+            time.sleep(1)
+
+if __name__ == '__main__':
+    t = timing()
