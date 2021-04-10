114ae35c804938662e935fb15b8b71cb81b7bf41
Luke Gavel <luke5799@users.noreply.github.com>
2021-04-10 17:00:00 -0400

    fixed window name

--- senv/tkm_test.py
+++ senv/tkm_test.py
@@ -13,7 +13,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
+        MainWindow.setObjectName("Track_Model_Window")
         MainWindow.resize(1037, 929)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
