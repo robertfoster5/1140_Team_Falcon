c493ad99cd6aa51740629cca83ebf6b15fb226d4
Luke Gavel <luke5799@users.noreply.github.com>
2021-04-25 13:24:11 -0400

    oops 3

--- senv/tkm_class.py
+++ senv/tkm_class.py
@@ -6,7 +6,7 @@ def kmhr_to_ms(num):
 	return num
 
 #total num
-train_num = 0
+train_num = []
 index = 0
 
 class Station:
@@ -428,15 +428,17 @@ class Track:
 	def add_train(self,letter):#n,way,block):
 		global train_num
 		global index
-		'''
+
 		if len(train_num) == 0:
 			n = 1
 			train_num.append(train_num)
 		else:
 			i = 0
 			while i < len(train_num):
+				if i+1 != train[i]:
+					n = i+1
+					train_num.append(
 		
-		'''
 		if self.line == "Red":
 			way = -1
 			blo = 9
@@ -606,11 +608,11 @@ class Track:
 			while i < len(self.train):
 				if self.blocks[int(self.train[i].block-1)].speed > 0:
 					if self.blocks[int(self.train[i].block-1)].speed < self.blocks[int(self.train[i].block-1)].s_limit:
-						signals.tkm_get_auth.emit(self.block[int(self.train[i].block-1)].auth)
+						signals.tkm_get_auth.emit(self.blocks[int(self.train[i].block-1)].auth)
 						signals.tkm_get_speed.emit(self.blocks[int(self.train[i].block-1)].speed)
 						#print(str(self.blocks[int(self.train[i].block-1)].speed)+" spppppppppppppppppppppppppppeeeeeeeeeeeeeeeeeeddddd")
 					else:
-						signals.tkm_get_auth.emit(self.block[int(self.train[i].block-1)].auth)
+						signals.tkm_get_auth.emit(self.blocks[int(self.train[i].block-1)].auth)
 						signals.tkm_get_speed.emit(self.blocks[int(self.train[i].block-1)].s_limit)
 						#print(str(self.blocks[int(self.train[i].block-1)].s_limit)+" spppppppppppppppppppppppppppeeeeeeeeeeeeeeeeeeddddd")
 				i = i+1
